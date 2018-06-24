c00e17979c651ed3bf10566fa0ecaa54c948e09f
sevki <s@sevki.org>
2018-06-24 22:50:40 +0100

    namespace: new package namespace
    namespace is meant to be a namespace. first implementation
    is just a dir, but later implentations in docker and plan9 namespaces
    are planned
    
    Signed-off-by: sevki <s@sevki.org>

---
+++ namespace/namespace.go
@@ -0,0 +1,26 @@
+package namespace
+
+import (
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	MREPL int = iota
+	MBEFORE
+	MAFTER
+)
+
+type Namespace string
+
+// Namespace is where builds are run, interface is the same as the plan9 namespaces
+func Bind(new, old string, flags int)  {}
+func Mount(new, old string, flags int) {}
+
+func New(name string) error {
+	if err := os.MkdirAll(name, 0644); err != nil {
+		return errors.Wrap(err, "new namespace")
+	}
+	return nil
+}
